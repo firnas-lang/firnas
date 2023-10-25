d90880381c45cf76442cef900550ee2cf7b4cc89
Hamza Jadid <haljadid@proximie.com>
2023-10-26 00:06:42 +0300

    feat: constant support

--- firnas_core/src/chunk.rs
+++ firnas_core/src/chunk.rs
@@ -1,16 +1,56 @@
+use crate::value::Value;
+use crate::value::ValueVec;
+
 pub enum OpCode {
-    OpReturn,
+    Return,
+    Constant,
 }
 
-pub struct Chunk(Vec<OpCode>);
+impl From<OpCode> for u8 {
+    fn from(val: OpCode) -> Self {
+        match val {
+            OpCode::Return => 0,
+            OpCode::Constant => 1,
+        }
+    }
+}
+
+impl From<u8> for OpCode {
+    fn from(val: u8) -> Self {
+        match val {
+            0 => OpCode::Return,
+            1 => OpCode::Constant,
+            _ => panic!("Undefined state"),
+        }
+    }
+}
+
+pub struct Chunk {
+    code: Vec<u8>,
+    constants: ValueVec,
+}
 
 impl Chunk {
     pub fn new() -> Self {
-        Self(Vec::new())
+        Self {
+            code: Vec::new(),
+            constants: ValueVec::new(),
+        }
     }
 
-    pub fn write(&mut self, byte: OpCode) {
-        self.0.push(byte);
+    pub fn write(&mut self, byte: u8) {
+        self.code.push(byte);
+    }
+
+    pub fn add_constant(&mut self, value: Value) -> usize {
+        self.constants.write(value);
+        self.constants.len() - 1
+    }
+}
+
+impl Default for Chunk {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -19,21 +59,31 @@ impl Chunk {
     pub fn disassemble_chunk(&self, name: &str) {
         println!("== {name} ==");
         let mut offset = 0;
-        while offset < self.0.len() {
+        while offset < self.code.len() {
             offset = self.disassemble_instruction(offset);
         }
     }
 
     fn disassemble_instruction(&self, offset: usize) -> usize {
         print!("{offset:04} ");
-        let instruction = self.0.get(offset).unwrap();
-        match instruction {
-            OpCode::OpReturn => Chunk::simple_instruction("OP_RETURN", offset),
+        let instruction = self.code.get(offset).unwrap();
+        match (*instruction).into() {
+            OpCode::Return => Chunk::simple_instruction("OP_RETURN", offset),
+            OpCode::Constant => Chunk::constant_instruction("OP_CONSTANT", self, offset),
         }
     }
 
     fn simple_instruction(name: &str, offset: usize) -> usize {
         println!("{name}");
         offset + 1
+    }
+
+    fn constant_instruction(name: &str, chunk: &Chunk, offset: usize) -> usize {
+        let constant = chunk.code.get(offset + 1).unwrap();
+        println!(
+            "{name:<16} {constant:04} '{}'",
+            chunk.constants.get(*constant as usize).unwrap()
+        );
+        offset + 2
     }
 }

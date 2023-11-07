c156457b0b55bb8f94b1d8b1499f6ff39cecbcd3
Hamza Jadid <haljadid@proximie.com>
2023-11-07 22:20:36 +0200

    test: added ar function tests

--- firnas_vm/src/stdlib/time.rs
+++ firnas_vm/src/stdlib/time.rs
@@ -1,9 +1,27 @@
 use crate::value;
+use crate::value::NativeFunction;
 use crate::virtual_machine;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
 
-pub fn clock(
+pub fn std_time_clock() -> (String, value::Value) {
+    let name = if cfg!(feature = "ar") {
+        String::from("ساعة")
+    } else {
+        String::from("clock")
+    };
+
+    (
+        name.clone(),
+        value::Value::NativeFunction(NativeFunction {
+            arity: 0,
+            name,
+            func: clock,
+        }),
+    )
+}
+
+fn clock(
     _interp: &mut virtual_machine::VirtualMachine,
     _args: &[value::Value],
 ) -> Result<value::Value, String> {
